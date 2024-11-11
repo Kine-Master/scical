9603e44774bc0c2cd8f867b2bd9f2fd608feed57
KrishnaMelromarc <143473789+KrishnaMelromarc@users.noreply.github.com>
2024-11-11 10:55:50 +0800

    Add project files.

---
+++ scical/Form1.Designer.vb
@@ -0,0 +1,338 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label3 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Button18 = New Button()
+        Button19 = New Button()
+        Button20 = New Button()
+        Button21 = New Button()
+        Button23 = New Button()
+        Button26 = New Button()
+        Button11 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(318, 60)
+        Label1.TabIndex = 0
+        ' 
+        ' Label3
+        ' 
+        Label3.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(12, 69)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(318, 60)
+        Label3.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(11, 274)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "0"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(11, 190)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 3
+        Button2.Text = "1"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(92, 190)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 4
+        Button3.Text = "2"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(11, 161)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 5
+        Button4.Text = "+"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(92, 161)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(75, 23)
+        Button5.TabIndex = 6
+        Button5.Text = "-"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(254, 190)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(75, 23)
+        Button6.TabIndex = 7
+        Button6.Text = "sin"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(173, 190)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(75, 23)
+        Button7.TabIndex = 8
+        Button7.Text = "3"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(11, 219)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(75, 23)
+        Button8.TabIndex = 9
+        Button8.Text = "4"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(92, 219)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(75, 23)
+        Button9.TabIndex = 10
+        Button9.Text = "5"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Location = New Point(254, 219)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(75, 23)
+        Button10.TabIndex = 11
+        Button10.Text = "cos"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Location = New Point(11, 132)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(75, 23)
+        Button12.TabIndex = 13
+        Button12.Text = "Mod"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.Location = New Point(173, 248)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(75, 23)
+        Button13.TabIndex = 14
+        Button13.Text = "9"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' Button14
+        ' 
+        Button14.Location = New Point(92, 245)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(75, 23)
+        Button14.TabIndex = 15
+        Button14.Text = "8"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Button15
+        ' 
+        Button15.Location = New Point(11, 245)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(75, 23)
+        Button15.TabIndex = 16
+        Button15.Text = "7"
+        Button15.UseVisualStyleBackColor = True
+        ' 
+        ' Button16
+        ' 
+        Button16.Location = New Point(173, 161)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(75, 23)
+        Button16.TabIndex = 17
+        Button16.Text = "*"
+        Button16.UseVisualStyleBackColor = True
+        ' 
+        ' Button17
+        ' 
+        Button17.Location = New Point(254, 248)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(75, 23)
+        Button17.TabIndex = 18
+        Button17.Text = "tan"
+        Button17.UseVisualStyleBackColor = True
+        ' 
+        ' Button18
+        ' 
+        Button18.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button18.Location = New Point(254, 274)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(75, 23)
+        Button18.TabIndex = 19
+        Button18.Text = "log"
+        Button18.UseVisualStyleBackColor = True
+        ' 
+        ' Button19
+        ' 
+        Button19.Location = New Point(173, 219)
+        Button19.Name = "Button19"
+        Button19.Size = New Size(75, 23)
+        Button19.TabIndex = 20
+        Button19.Text = "6"
+        Button19.UseVisualStyleBackColor = True
+        ' 
+        ' Button20
+        ' 
+        Button20.Location = New Point(254, 161)
+        Button20.Name = "Button20"
+        Button20.Size = New Size(75, 23)
+        Button20.TabIndex = 21
+        Button20.Text = "/"
+        Button20.UseVisualStyleBackColor = True
+        ' 
+        ' Button21
+        ' 
+        Button21.Location = New Point(92, 274)
+        Button21.Name = "Button21"
+        Button21.Size = New Size(75, 23)
+        Button21.TabIndex = 22
+        Button21.Text = "CE"
+        Button21.UseVisualStyleBackColor = True
+        ' 
+        ' Button23
+        ' 
+        Button23.Location = New Point(92, 132)
+        Button23.Name = "Button23"
+        Button23.Size = New Size(75, 23)
+        Button23.TabIndex = 24
+        Button23.Text = "Exp"
+        Button23.UseVisualStyleBackColor = True
+        ' 
+        ' Button26
+        ' 
+        Button26.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button26.Location = New Point(173, 274)
+        Button26.Name = "Button26"
+        Button26.Size = New Size(75, 23)
+        Button26.TabIndex = 27
+        Button26.Text = "="
+        Button26.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Location = New Point(173, 132)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(75, 23)
+        Button11.TabIndex = 28
+        Button11.Text = "Bin"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(335, 313)
+        Controls.Add(Button26)
+        Controls.Add(Button11)
+        Controls.Add(Button23)
+        Controls.Add(Button21)
+        Controls.Add(Button20)
+        Controls.Add(Button19)
+        Controls.Add(Button18)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label3)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents Button23 As Button
+    Friend WithEvents Button26 As Button
+    Friend WithEvents Button11 As Button
+
+End Class
---
+++ scical/Form1.vb
@@ -0,0 +1,113 @@
+﻿Imports System.Reflection.Emit
+
+Public Class Form1
+
+    Dim f As Double
+    Dim s As Double
+    Dim a As Double
+    Dim op As String
+    Dim n As Int32
+
+    ' Number input buttons
+    Private Sub Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click, Button9.Click, Button8.Click, Button7.Click, Button3.Click, Button2.Click, Button19.Click, Button15.Click, Button14.Click, Button13.Click
+        Dim b As Button = sender
+        If Label1.Text = "0" Then
+            Label1.Text = b.Text
+        Else
+            Label1.Text = Label1.Text + b.Text
+        End If
+    End Sub
+
+    ' Clear button (C)
+    Private Sub Button21_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button21.Click
+        Label1.Text = "0"
+        Label3.Text = ""
+    End Sub
+
+    ' Arithmetic operation buttons (+, -, *, /, Mod, Exp)
+    Private Sub arithmatic_function(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click, Button5.Click, Button23.Click, Button20.Click, Button16.Click, Button12.Click
+        Dim ops As Button = sender
+        f = Label1.Text
+        Label3.Text = Label1.Text
+        Label1.Text = ""
+        op = ops.Text
+        Label3.Text = Label3.Text + " " + op
+    End Sub
+
+    ' Equals button (=)
+    Private Sub Button26_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button26.Click
+        Try
+            s = Val(Label1.Text)
+
+            Select Case op
+                Case "+"
+                    a = f + s
+                Case "-"
+                    a = f - s
+                Case "*"
+                    a = f * s
+                Case "/"
+                    If s = 0 Then
+                        Throw New DivideByZeroException("Cannot divide by zero.")
+                    End If
+                    a = f / s
+                Case "Mod"
+                    a = f Mod s
+                Case "Exp"
+                    a = f ^ s
+            End Select
+
+            Label1.Text = a.ToString()
+            Label3.Text = ""
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+
+    ' Binary conversion (Dec -> Bin)
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        If Int32.TryParse(Label1.Text, n) Then
+            Label1.Text = Convert.ToString(n, 2)
+        Else
+            Label1.Text = ""
+        End If
+    End Sub
+
+    ' Scientific functions (Sin, Cos, Tan, Log)
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        a = Math.Sin(Label1.Text)
+        Label1.Text = a
+        Label3.Text = ""
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        a = Math.Cos(Label1.Text)
+        Label1.Text = a
+        Label3.Text = ""
+    End Sub
+
+    Private Sub Button17_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button17.Click
+        a = Math.Tan(Label1.Text)
+        Label1.Text = a
+        Label3.Text = ""
+    End Sub
+
+    Private Sub Button18_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button18.Click
+        a = Math.Log(Label1.Text)
+        Label1.Text = a
+        Label3.Text = ""
+    End Sub
+
+    ' Load event to set the scientific calculator layout
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Text = "Scientific Calculator"
+        Me.Height = 423
+        Me.Width = 393
+        Button26.Width = 75
+        Button26.Height = 23
+    End Sub
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
